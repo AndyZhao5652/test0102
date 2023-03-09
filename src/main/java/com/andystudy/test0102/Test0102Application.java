90311e5182257763dfc0dbb8a9cb4c14dd3eb518
AndyZhao5652 <54948555@qq.com>
2023-03-09 15:12:00 +0800

    Init

---
+++ src/main/java/com/andystudy/test0102/Test0102Application.java
@@ -0,0 +1,13 @@
+package com.andystudy.test0102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0102Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test0102Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/andystudy/test0102/Test0102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andystudy.test0102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0102ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
